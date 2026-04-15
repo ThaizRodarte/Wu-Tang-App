b8bee10e4b2a0d776ea08bad9ff074b65d760c61
ThaizRodarte <thaizrodarte08@gmail.com>
2026-04-15 13:06:33 -0700

    Initial Commit

---
+++ Wu-Tang App/Wu_Tang_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wu_Tang_AppApp.swift
+//  Wu-Tang App
+//
+//  Created by RODARTE, THAIZ on 4/15/26.
+//
+
+import SwiftUI
+
+@main
+struct Wu_Tang_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
